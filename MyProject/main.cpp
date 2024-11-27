9e49b30275cf5f775bcd191035eae48c819ab9da
Miroslav262 <miroslavchik235@gmail.com>
2024-11-27 21:37:46 +0300

    Добавлен вывод общей информации о задании

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #9. Library Subscription\n";
+    cout << "Author: Markushevski Miraslau\n";
     return 0;
 }
